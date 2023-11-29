09d6aac5126b69255cbe0241b0d4a41588bd020e
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-11-29 22:46:35 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section-context";
+import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
+import ThemeContextProvider from "@/context/theme-context";
+import { Toaster } from "react-hot-toast";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Collins Nwoko | Personal Portfolio",
+  description: "Collins nwoko is a full-stack developer with 4 years of experience.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-slate-50 text-slate-900 relative pt-28 sm:pt-36 dark:bg-[#070427] dark:text-gray-50 dark:text-opacity-90`}
+      >
+        <div className="bg-[#cee4aa] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#9e3c96]">
+
+        </div>
+        <div className="bg-[#bab3f7] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#090444]"></div>
+        {/* <div className="bg-[#f3eaeb] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#7c2555]">
+
+        </div>
+        <div className="bg-[#bab3f7] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#090444]"></div> */}
+        {/* <div className="bg-[#bab3f7] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#070427]"></div> */}
+
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
+      </body>
+    </html>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,115 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+import snapgram from "@/public/snapgram.png";
+import dallasville from "@/public/dallasville.png";
+import tojays from "@/public/tojays.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+
+  {
+    title: "Software Developer",
+    location: "Skillz Systems Ltd, Nigeria",
+    description:
+      "I'm now working with the following stack: React, Nodejs, Laravel and Docker to build microservices. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2022 - present",
+  },
+
+  {
+    title: "Fullstack Developer",
+    location: "FCT, Nigeria",
+    description:
+      "I worked as a fullstack developer for a year under contract. I also upskilled to by  learning Laravel",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022",
+  },
+  {
+    title: "Freelancer Web Developer",
+    location: "FCT, Nigeria",
+    description:
+      "Designed and developed websites based on client requirements as a freelancer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2020 - 2022",
+  }
+] as const;
+
+export const projectsData = [
+  {
+    title: "Dallasvile",
+    description:
+      "Dallasville is a leading real estate company dedicated to helping individuals and families find their perfect homes in Nigeria",
+    tags: ["React", "Typescript", "MongoDB", "Tailwind", "Nodejs"],
+    imageUrl: dallasville,
+    link: 'https://dallasville.vercel.app/',
+  },
+  {
+    title: "Torjays",
+    description:
+      "Tojay Chemical is a paint production company offering reliable, efficient and quality paint production, sales and delivery services, alongside other related services",
+    tags: ["React", "TypeScript", "Vercel", "Tailwind", "Redux"],
+    imageUrl: tojays,
+    link: 'https://tojays.vercel.app/',
+  },
+  {
+    title: "SnapGram",
+    description:
+      "A public social web app, to share, link and post images ",
+    tags: ["React", "Typescript", "Appwrite", "Tailwind", "React Query"],
+    imageUrl: snapgram,
+    link: 'https://social-app-sand.vercel.app/',
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "SCSS",
+  "PHP",
+  "Laravel",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "MySQL",
+] as const;
